4720c46236713aca9d39c91f0d50cbad3613f121
Martin Brugger <martin@brugger.eu>
2015-12-27 17:37:33 +0100

    changed to webroot authentication method

--- request_certificate.sh
+++ request_certificate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-docker run -it --rm -p 1086:80 -p 1087:443 --name letsencrypt \
-            -v "/etc/letsencrypt:/etc/letsencrypt" \
-            -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-            quay.io/letsencrypt/letsencrypt:latest auth --renew-by-default --server \
-      https://acme-v01.api.letsencrypt.org/directory -d my.example.com
+mkdir -p /tmp/letsencrypt/www
+docker run -it --rm --name letsencrypt \
+    -v "/etc/letsencrypt:/etc/letsencrypt" \
+    -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+    -v "/tmp/letsencrypt/www:/var/www" \
+    quay.io/letsencrypt/letsencrypt:latest auth --authenticator webroot --webroot-path /var/www --renew-by-default --server \
+    https://acme-v01.api.letsencrypt.org/directory -d my.example.com
